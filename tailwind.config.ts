a7627b5f013c571c8c78c87e3dc9c03b302b2d41
Tiago Silva <neiva_tiago@hotmail.com>
2024-10-23 21:59:30 +0100

    change tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,44 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate"; // Change require to import
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  // Correctly set darkMode as a string
+  darkMode: "class",  // Change this from an array to a string
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	container: {
-  		center: 'true',
-  		padding: '15px'
-  	},
-  	screens: {
-  		sm: '640px',
-  		md: '768px',
-  		lg: '960px',
-  		xl: '1200px'
-  	},
-  	fontFamily: {
-  		primary: 'var(--font-jetbrainsMono)'
-  	},
-  	extend: {
-  		colors: {
-  			primary: '#1c1c22',
-  			accent: {
-  				DEFAULT: '#00ff99',
-  				hover: '#00e187'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    container: {
+      center: true, // Change from 'true' (string) to true (boolean)
+      padding: '15px',
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    fontFamily: {
+      primary: 'var(--font-jetbrainsMono)',
+    },
+    extend: {
+      colors: {
+        primary: '#1c1c22',
+        accent: {
+          DEFAULT: '#00ff99',
+          hover: '#00e187',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
+
 export default config;
