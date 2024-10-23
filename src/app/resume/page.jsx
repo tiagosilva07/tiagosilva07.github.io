2410ddba0f166112e9f6c727eb2a0a5c80d388db
Tiago Silva <neiva_tiago@hotmail.com>
2024-10-23 21:33:10 +0100

    create pages and navigation

---
+++ src/app/resume/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Resume page</div>
+  )
+}
+
+export default page
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py:12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* brand */}
+
+        <Link href="/">
+        <h1 className="text-4xl font-semibold">
+          Tiago Silva<span className="text-accent">.</span>
+        </h1>
+        </Link>
+        {/* Desktop Navigation */}
+        <div className="hidden xl:flex items-center gap-8">
+        <Nav />
+        </div>
+        {/*mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav/>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
